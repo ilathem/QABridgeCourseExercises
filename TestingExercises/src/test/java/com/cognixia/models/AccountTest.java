c5ab415519958f9148a41e1da49585991448d160
Charles Dunn <crdunn210@gmail.com>
2025-01-16 10:48:58 -0500

    Update AccountTest.java

--- TestingExercises/src/test/java/com/cognixia/models/AccountTest.java
+++ TestingExercises/src/test/java/com/cognixia/models/AccountTest.java
@@ -1,5 +1,10 @@
 package com.cognixia.models;
 
 public class AccountTest {
+  //1. As a member, I should be able to view a list of my past claims so I can verify my claim status
+
+	//2. As a user, I should be able to search claims within a range of dates
+
+	//3. As a provider, I should be able to upload several claims at once and add them to the database
 
 }
